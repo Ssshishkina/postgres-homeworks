b3e00a3e8bdf1f65b8adeaa2e0c2114fc9780071
nastya shishkina <ana.koze@yandex.ru>
2024-01-08 22:57:58 +0400

    выполнено задание 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+    employee_id int PRIMARY KEY,
+    first_name varchar(15),
+    last_name varchar(20),
+    title varchar(50),
+    birth_date date,
+    notes text
+);
+
+CREATE TABLE customers
+(
+    customer_id varchar(5) PRIMARY KEY,
+    company_name varchar(50),
+    contact_name varchar(50)
+);
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+    customer_id varchar(10),
+    employee_id int,
+    order_date date,
+    ship_city varchar(50)
+);
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,55 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import os
+import psycopg2
+
+current_directory = os.getcwd()
+file_path_employees = os.path.join(current_directory, 'north_data', 'employees_data.csv')
+file_path_customers = os.path.join(current_directory, 'north_data', 'customers_data.csv')
+file_path_orders = os.path.join(current_directory, 'north_data', 'orders_data.csv')
+
+
+class DataLoader:
+    """Класс для загрузки данных из CSV-файла в базу данных."""
+    def __init__(self, csv_file, table_name):
+        self.csv_file = csv_file
+        self.table_name = table_name
+
+    def connect_to_database(self):
+        """Устанавливает соединение с базой данных."""
+        conn = psycopg2.connect(
+            host="localhost",
+            database="north",
+            user="postgres",
+            password="4242")
+        return conn
+
+    def insert_data(self):
+        """Загружает данные из файла CSV в указанную таблицу."""
+        conn = self.connect_to_database()
+        cursor = conn.cursor()
+        try:
+            with open(self.csv_file, 'r') as file:
+                reader = csv.reader(file)
+                next(reader)  # Пропуск заголовка
+                for row in reader:
+                    placeholders = ', '.join(['%s'] * len(row))
+                    query = f"INSERT INTO {self.table_name} VALUES ({placeholders})"
+                    cursor.execute(query, row)
+                conn.commit()
+            print(f"Данные из файла успешно загружены в таблицу")
+        except (psycopg2.Error, csv.Error) as e:
+            print(f"Произошла ошибка при загрузке данных: {str(e)}")
+        finally:
+            cursor.close()
+            conn.close()
+
+data_loader = DataLoader(file_path_employees, 'employees')
+data_loader.insert_data()
+
+data_loader_2 = DataLoader(file_path_customers, 'customers')
+data_loader_2.insert_data()
+
+data_loader_3 = DataLoader(file_path_orders, 'orders')
+data_loader_3.insert_data()
+
